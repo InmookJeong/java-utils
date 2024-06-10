d23c2203126e80e71ce2f0664dee192eaaeb69fc
InmookJeong <jeonginmook@gmail.com>
2024-06-10 22:59:58 +0900

    feat - Add nullToString and getRandomString method
    Add nullToString with default value method.
    Add getRandomString Method.

--- Utils/src/main/kr/mook/datatype/StringUtil.java
+++ Utils/src/main/kr/mook/datatype/StringUtil.java
@@ -1,5 +1,7 @@
 package kr.mook.datatype;
 
+import java.util.Random;
+
 /**
  * StringUtil
  * 
@@ -8,6 +10,11 @@ package kr.mook.datatype;
  * @version 1.0.0
  */
 public class StringUtil {
+	
+	private static final String ALPHABET_UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String ALPHABET_LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+	private static final String NUMBERS = "0123456789";
+	private static final String SPECIAL_CHARACTERS = "!@#$_-";
 	
 	/**
 	 * This method to check if value is empty or null.<br/>
@@ -42,5 +49,38 @@ public class StringUtil {
 	public static String nullToString(final String value) {
 		if(value == null) return "";
 		return value;
+	}
+	
+	/**
+	 * This method checks if the string is null and returns an default value if the string is null, otherwise it returns the passed value.
+	 * 
+	 * @param value
+	 * @return empty string or parameter value
+	 */
+	public static String nullToString(final String value, final String defaultValue) {
+		if(value == null) return defaultValue;
+		return value;
+	}
+	
+	/**
+	 * When you enter a length, this method creates a string that randomly combines English letters, numbers, and special characters to the length entered.
+	 * 
+	 * @param length
+	 * @return
+	 */
+	public static String getRandomString(final int length) {
+		
+		String characters = ALPHABET_UPPERCASE_LETTERS
+				+ ALPHABET_LOWERCASE_LETTERS
+				+ NUMBERS
+				+ SPECIAL_CHARACTERS;
+		StringBuilder result = new StringBuilder(length);
+		Random random = new Random();
+		
+		for (int i = 0; i < length; i++) {
+			result.append(characters.charAt(random.nextInt(characters.length())));
+		}
+		
+		return result.toString();
 	}
 }
--- Utils/src/test/kr/mook/datatype/StringUtilTest.java
+++ Utils/src/test/kr/mook/datatype/StringUtilTest.java
@@ -53,5 +53,29 @@ class StringUtilTest {
 	void nullToString(String data, String expected) {
 		assertEquals(StringUtil.nullToString(data), expected);
 	}
+	
+	@DisplayName(value = "문자열의 값이 Null이면 중간 값(default value)을 반환하고 Null이 아니면 전달받은 값을 반환하는지 확인")
+	@ParameterizedTest
+	@CsvSource({
+		", 'a', 'a'",
+		"'', 'b', ''",
+		"abc, abc, abc",
+		"홍길동, 홍길동, 홍길동"
+	})
+	void nullToString(String data, String defaultValue, String expected) {
+		assertEquals(StringUtil.nullToString(data, defaultValue), expected);
+	}
+	
+	@DisplayName(value = "영문자, 숫자, 특수문자로 랜덤하게 만들어진 문자열의 길이(length)가 예상되는 값(expected)과 일치하는지 확인")
+	@ParameterizedTest
+	@CsvSource({
+		"10, 10",
+		"1, 1",
+		"15, 15",
+		"7, 7"
+	})
+	void getRandomString(int length, int expected) {
+		assertEquals(StringUtil.getRandomString(length).length(), expected);
+	}
 
 }
